629037477591877d6198a85e5b45506058259c65
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-07-06 20:29:53 +0530

    Initialized frontend of the project

---
+++ client/src/components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="bg-primary text-white p-3">
+            <h1 className='text-center'>Header Comp</h1>
+        </div>
+    )
+}
+
+export default Header
