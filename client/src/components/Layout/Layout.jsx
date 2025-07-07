78bbd80c1454d2f65283189652c43f671e79567e
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-07-07 10:54:42 +0530

    Done

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import Footer from './Footer';
 import Header from './Header';
-import { HelmetProvider } from 'react-helmet-async'
-// import { HelmetProvider } from 'react-helmet-async'; // ✅ THIS IS MISSING
 
-
-const Layout = ({ children, title, description, keywords, author }) => {
+const Layout = (props) => {
     return (
         <div>
-            <HelmetProvider>
-                <meta charSet="utf-8" />
-                <meta name="description" content={description} />
-                <meta name="keywords" content={keywords} />
-                <meta name="author" content={author} />
-                <title>My Title</title>
-            </HelmetProvider>
-
             <Header />
             <main style={{ minHeight: "70vh" }}>
-                {children}
+                {props.children}
             </main>
             <Footer />
         </div>
