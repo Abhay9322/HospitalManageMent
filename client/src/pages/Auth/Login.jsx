09f0523c6f9e26cfa14f6f2caf7c6a64b4f88989
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-07-10 17:13:57 +0530

    Added login nd logout logic

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import toast from 'react-hot-toast';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { data, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/auth';
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [auth, setAuth] = useAuth();
 
     const navigate = useNavigate();
 
@@ -25,6 +27,12 @@ const Login = () => {
 
             if (res && res.data.success) {
                 toast.success(res.data.message);
+                setAuth({
+                    ...auth,
+                    user: res.data.user,
+                    token: res.data.token
+                })
+                localStorage.setItem('auth', JSON.stringify(res.data))
                 navigate('/');
             } else {
                 toast.error(res.data.message);
